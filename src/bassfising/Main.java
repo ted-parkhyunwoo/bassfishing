b9436ef0486b9d7243facb7fce8d33e96e66e029
ted-macbookAir <zoogree@gmail.com>
2025-10-13 05:12:50 +0900

    주석 수정과 README 개선

--- src/bassfising/Main.java
+++ src/bassfising/Main.java
@@ -11,13 +11,13 @@ public class Main {
     public static void main(String[] args) {
         // !주의: Sinker,Line convert의 두번 째 매개변수는 현재 값을 넣습니다. (3호: 3, ho) -> 자동으로 gram, lb로 전환
         // 싱커 호수<->gram 상호 변환기
-        System.out.println(Sinker.convert(3, ho));     // 11.25 (double)
+        System.out.println(Sinker.convert(3, ho));     // 11.25 (data type:double)
         Sinker.printConvert(14, g);                    // 14.0g: 3.73호
         Sinker.printConvert(5, ho);                    // 5.0ho: 18.75g
 
 
         // 라인 호수<->lb 상호 변환기
-        System.out.println(Line.convert(4, ho));       // 16.0  (double)
+        System.out.println(Line.convert(4, ho));       // 16.0  (data type:double)
         Line.printConvert(12, lb);                     // 12.0lb: 3.0호
 
 
@@ -27,13 +27,13 @@ public class Main {
         
         // myZillion21의 12lb는 몇미터 감기는지 확인
         double res = myZillion21.getOtherLineCapa(12, lb);
-        System.out.println(res);                        // 93.33333...
+        System.out.println(res);                        // 105.0
         
         // 쉬운출력
-        myZillion21.printOtherLine(16, lb);             // 16.0lb 70.0m
+        myZillion21.printOtherLine(16, lb);             // 16lb 78.8m
 
         // 쉬운출력2 - 자주 쓰이는 라인 전체출력 (4lb ~ 20lb)
-        myZillion21.setName("21질리언");
+        myZillion21.setName("21질리언");    // 기본초기화: "empty"
         myZillion21.printOtherLine();
         /* print.
             21질리언:
