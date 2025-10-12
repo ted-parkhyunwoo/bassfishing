6c452c2219e32ffca0496259e028d19f60ee184e
ted-macbookAir <zoogree@gmail.com>
2025-10-13 04:16:25 +0900

    ReelLineCapa 기능추가: printOtherLine

--- src/bassfising/Main.java
+++ src/bassfising/Main.java
@@ -20,10 +20,29 @@ public class Main {
 
         // 릴 기본제원 권사량을 베이스로 다른파워의 라인 권사량 추정.
         // myZillion21 은 14lb 80m 감기는 릴.
-        ReelLineCapa myZillion21 = new ReelLineCapa(14, lb, 80);
+        ReelLineCapa myZillion21 = new ReelLineCapa(14, lb, 90);
         // myZillion21의 12lb는 몇미터 감기는지 확인
         double res = myZillion21.getOtherLineCapa(12, lb);
-        System.out.println(res);                                // 93.33333...
+        System.out.println(res);                        // 93.33333...
+        // 쉬운출력
+        myZillion21.printOtherLine(16, lb);             // 16.0lb 70.0m
+
+        // 이름변경: 기본생성-"empty"
+        myZillion21.setName("21질리언");
+        // 자주쓰는 라인 4lb~20lb 모두 출력
+        myZillion21.printOtherLine();
+        /* print.
+            21질리언:
+              4.0lb 315.00m  오차주의: 기본(14.00lb)보다 6lb 이상 차이.
+              6.0lb 210.00m  오차주의: 기본(14.00lb)보다 6lb 이상 차이.
+              8.0lb 157.50m  오차주의: 기본(14.00lb)보다 6lb 이상 차이.
+              10.0lb 126.00m
+              12.0lb 105.00m
+              14.0lb 90.00m (기본값)
+              16.0lb 78.75m
+              18.0lb 70.00m
+              20.0lb 63.00m  오차주의: 기본(14.00lb)보다 6lb 이상 차이.
+         */
 
     }
 }
--- src/bassfising/utility/ReelLineCapa.java
+++ src/bassfising/utility/ReelLineCapa.java
@@ -1,8 +1,6 @@
 package bassfising.utility;
 import bassfising.utility.converter.Line;
 
-// 합사는 lb 사용이 거의 불가능할정도로 조구사마다 스펙 상이하므로 호수만 고려합니다.
-
 /*  class LineUnitChecker
 DESC: 입력된 라인의 유닛검증 Class. String이 그저 "ho" 혹은 "lb" 인지만 검증합니다.
 e.g: 
@@ -22,13 +20,22 @@ e.g:
     사용: myReel1.getOtherLineCapa(12, "lb")  -> 93.33333           - 12lb 라인 변경시 93.33333m 감을수 있다는 뜻
 */
 public class ReelLineCapa {
+    // 기본 길이 단위 상수.
+    public static final String LENGTH_UNIT = "m";
+
+    private String name;
     private double baseLength;
     // 한쪽만 입력해도 생성자에서 자동으로 채워넣음.
     private double basePowerLb;
     private double basePowerHo;
-    
-    public void setLength(double baseLength)     { this.baseLength = baseLength; }
-    protected double getLength()                 { return this.baseLength; }
+    { this.name = "empty"; }
+
+    public void setLength(double baseLength)        { this.baseLength = baseLength; }
+    protected double getLength()                    { return this.baseLength; }
+    public double getBasePowerLb()                  { return this.basePowerLb; }
+    public void setName(String name)                { this.name = name; }
+    public String getName()                         { return this.name; }
+
 
     // desc:    init all member values method.
     // e.g:     14lb 80m 라 치면 setBaseInfo(14, "lb", 80) 으로 입력.
@@ -48,7 +55,11 @@ public class ReelLineCapa {
     public ReelLineCapa(double basePower, String lineUnit, double baseLength) {
         setBaseInfo(basePower, lineUnit, baseLength);
     }
-
+    // 이름을 추가한 생성자 오버로딩
+    public ReelLineCapa(String name, double basePower, String lineUnit, double baseLength) {
+        setBaseInfo(basePower, lineUnit, baseLength);
+        setName(name);
+    }
     // targetPower:double 는 변경할 라인의 파워. lineUnit은 단위이며, "ho" 혹은 "lb".
     public double getOtherLineCapa(double tergetPower, String lineUnit) {
         if (this.basePowerLb == 0 || this.baseLength == 0) {
@@ -66,6 +77,32 @@ public class ReelLineCapa {
         }
             
         return 0f;
+    }
+
+    // simple print other power line.
+    public void printOtherLine(double targetPower, String lineUnit) {
+        double res = getOtherLineCapa(targetPower, lineUnit);
+        String basePowerStr = String.format("%.2f", getBasePowerLb());
+        String resStr = String.format("%.2f", res);
+        System.out.print(targetPower + "lb " + resStr + LENGTH_UNIT);
+        if (getBasePowerLb() >= targetPower + 6 || getBasePowerLb() <= targetPower - 6) {
+            System.out.print("  오차주의: 기본(" + basePowerStr + "lb)보다 6lb 이상 차이.");
+        }
+        if (getBasePowerLb() == targetPower) {
+            System.out.print(" (기본값)");
+        }
+        System.out.println();
+    }
+
+    // 자주쓰는 lb 4~20 까지 모두 출력
+    public void printOtherLine() {
+        int startPoint = 4;
+        int endPoint = 20;
+        System.out.println(getName() + ":");
+        for (int i = startPoint; i <= endPoint; i += 2) {
+            System.out.print("  ");
+            printOtherLine(i, "lb");
+        }
     }
 }
 
