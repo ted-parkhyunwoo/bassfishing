376f8870a1a8d7cc9fb01257cec8defa583e145e
ted-desktop-win <zoogree@gmail.com>
2025-10-13 22:59:30 +0900

    Major update. ReelLineCapa의 'ho'사용시 올바르지 않게 출력되는 버그 수정. Main.java -> example.java로 수정. LF스타일에 맞게 수정. printOtherLine 출력문 조정(lb와 ho가 이제 같이 표기됨)

--- src/bassfising/example.java
+++ src/bassfising/example.java
@@ -3,7 +3,7 @@ import bassfising.utility.ReelLineCapa;
 import bassfising.utility.converter.Line;
 import bassfising.utility.converter.Sinker;
 
-public class Main {
+public class example {
     // UNIT. 직접 String으로 사용해도 됨.
     static String lb = "lb";
     static String ho = "ho";
@@ -25,29 +25,31 @@ public class Main {
         // 릴 기본제원 권사량을 베이스로 다른파워의 라인 권사량 추정.
         // 초기화: myZillion21 은 14lb 90m 감기는 릴.
         ReelLineCapa myZillion21 = new ReelLineCapa(14, lb, 90);
+        ReelLineCapa iWant = new ReelLineCapa("21SteezeLtd", 3.5, ho, 85);   //  이름 초기화 가능. 기본값:"Unknown";
         
         // myZillion21의 12lb는 몇미터 감기는지 return.
         double res = myZillion21.getOtherLineCapa(12, lb);
         System.out.println(res);                        // 105.0
         
-        // 쉬운출력
-        myZillion21.printOtherLine(16, lb);             // 16lb 78.8m
+        // 쉬운출력 - 단일 특정라인 권사량 추정 출력.
+        myZillion21.printOtherLine(16, lb);             // 4.0ho-16lb 78.8m
+        iWant.printOtherLine(3, ho);                    // 3.0ho-12lb 99.2m
 
         // 쉬운출력2 - 자주 쓰이는 라인 전체출력 (4lb ~ 20lb)
-        myZillion21.setName("21질리언");    // 기본초기화: "empty"
+        myZillion21.setName("21질리언");
         myZillion21.printOtherLine();
         /* print.
-            21질리언:
-                4lb 315.0m  오차주의: 기본 14lb과 6lb 이상 차이남
-                6lb 210.0m  오차주의: 기본 14lb과 6lb 이상 차이남
-                8lb 157.5m  오차주의: 기본 14lb과 6lb 이상 차이남
-                10lb 126.0m
-                12lb 105.0m
-                14lb 90.0m (기본값)
-                16lb 78.8m
-                18lb 70.0m
-                20lb 63.0m  오차주의: 기본 14lb과 6lb 이상 차이남
+        21질리언:
+            1.0ho-4lb 315.0m (오차주의)
+            1.5ho-6lb 210.0m (오차주의)
+            2.0ho-8lb 157.5m (오차주의)
+            2.5ho-10lb 126.0m
+            3.0ho-12lb 105.0m
+            3.5ho-14lb 90.0m (Base)
+            4.0ho-16lb 78.8m
+            4.5ho-18lb 70.0m
+            5.0ho-20lb 63.0m (오차주의)
          */
-
+        iWant.printOtherLine();
     }
 }
--- src/bassfising/utility/ReelLineCapa.java
+++ src/bassfising/utility/ReelLineCapa.java
@@ -1,10 +1,11 @@
 package bassfising.utility;
 import bassfising.utility.converter.Line;
 
+
 /*  class LineUnitChecker
-DESC: 입력된 라인의 유닛검증 Class. String이 그저 "ho" 혹은 "lb" 인지만 검증합니다.
-e.g: 
-    LineUnitChecker.check(라인단위:String) -> return true(or false) 합니다.
+    DESC: 입력된 라인의 유닛검증 Class. String이 그저 "ho" 혹은 "lb" 인지만 검증합니다.
+    e.g: 
+        LineUnitChecker.check(라인단위:String) -> return true(or false) 합니다.
 */
 class LineUnitChecker {
     public static boolean check(String token)   { return token == "lb" || token == "ho"; }
@@ -12,32 +13,35 @@ class LineUnitChecker {
 
 
 /*  class ReelLineCapa
-DESC: 기본 권사량 정보를 입력하면 다른라인파워의 권사량을 보여줍니다. 매개변수가 있는 생성자 사용을 권장.
-dependency: LineUnitChecker(내부), LineConverter(외부)
-
-e.g: 
-    초기화: ReelLineCapa myReel1 = new ReelLineCapa(14, "lb", 80);  - 기본정보 14lb 80m 감기는 릴이라는 뜻
-    사용: myReel1.getOtherLineCapa(12, "lb")  -> 93.33333           - 12lb 라인 변경시 93.33333m 감을수 있다는 뜻
+    DESC: 기본 권사량 정보를 입력하면 다른라인파워의 권사량을 보여줍니다. 매개변수가 있는 생성자 사용을 권장.
+    dependency: LineUnitChecker(내부), LineConverter(외부)
+    e.g: 
+        초기화: ReelLineCapa myReel1 = new ReelLineCapa(14, "lb", 80);  - 기본정보 14lb 80m 권사량을 지정.
+        사용: myReel1.getOtherLineCapa(12, "lb")  -> 93.33333(double)   - 12lb 라인 변경시 93.33333m 권사량
+        Print: myReel1.printOtherLine(12, "lb")   -> 3.0ho-12lb 93.3m(void print)   -  3호, 12파운드는 93.3m 권사량
 */
 public class ReelLineCapa {
     // 기본 길이 단위 상수.
     public static final String LENGTH_UNIT = "m";
+    // 오차보정(printOtherLine 메서드가 기본프로필의 lb보다 새로감을 라인의 lb가 얼마나 차이나면 "(오차주의)"경고문을 띄울지 결정). 
+    public static final int DEFFERENCE_OVER = 6;    // 6lb 기본값.
 
-    private String name;
+    private String name = "Unknown";
     private double baseLength;
-    // 한쪽만 입력해도 생성자에서 자동으로 채워넣음.
+
+    // 한쪽만 입력해도 생성자(정확히는 setBaseInfo)에서 자동으로 채워넣음.
     private double basePowerLb;
     private double basePowerHo;
-    { this.name = "empty"; }
 
-    public void setLength(double baseLength)        { this.baseLength = baseLength; }
-    protected double getLength()                    { return this.baseLength; }
-    public double getBasePowerLb()                  { return this.basePowerLb; }
-    public void setName(String name)                { this.name = name; }
-    public String getName()                         { return this.name; }
+    // getter setter.
+    private double getLength()                       { return this.baseLength; }
+    private double getBasePowerLb()                  { return this.basePowerLb; }
+    private double getBasePowerHo()                  { return this.basePowerHo; }
+    public void setName(String name)                 { this.name = name; }
+    public String getName()                          { return this.name; }
+    
 
-
-    // desc:    init all member values method.
+    // desc:    init all member values method. 생성자에 쓰일 기본 초기화메서드. 몇몇 멤버변수를 초기화함.
     // e.g:     14lb 80m 라 치면 setBaseInfo(14, "lb", 80) 으로 입력.
     public void setBaseInfo(double basePower, String lineUnit, double baseLength) {
         // Todo. error control.
@@ -48,7 +52,7 @@ public class ReelLineCapa {
         this.baseLength = baseLength;
         boolean isLbUnit = lineUnit == "lb"? true: false;
         this.basePowerLb = (isLbUnit)? basePower : Line.convert(basePower, "ho");
-        this.basePowerHo = (isLbUnit)? Line.convert(basePower, "ho") : basePower;        
+        this.basePowerHo = (isLbUnit)? Line.convert(basePower, "lb") : basePower;   // debug-251013: 논리오류 수정. "ho" -> "lb" 로 교체
     }
 
     // Constructor. setBaseInfo를 호출
@@ -60,8 +64,11 @@ public class ReelLineCapa {
         setBaseInfo(basePower, lineUnit, baseLength);
         setName(name);
     }
+
+    
     // targetPower:double 는 변경할 라인의 파워. lineUnit은 단위이며, "ho" 혹은 "lb".
     public double getOtherLineCapa(double tergetPower, String lineUnit) {
+        // TODO 예외 컨트롤.
         if (this.basePowerLb == 0 || this.baseLength == 0) {
             System.out.println("[ERR]initialize was wrong.");
             return -1;
@@ -76,24 +83,33 @@ public class ReelLineCapa {
             default: System.out.println("[Error] Line Type was wrong.");
         }
             
-        return 0f;
+        return -1f;
+    }
+    
+
+    // 이하 멤버메서드: 쉬운 출력문. 
+    // simple print other power line
+    public void printOtherLine(double newPower, String lineUnit) {
+        // double -> rounded Strings. 필요에 맞춰 모두 반올림처리됨.
+        String newCapaStr = String.format("%.1f",  getOtherLineCapa(newPower, lineUnit));
+        String newPowerLbStr = lineUnit == "lb"? String.format("%.0f", newPower) + "lb" : String.format("%.0f", Line.convert(newPower, "ho")) + "lb";
+        String newPowerHoStr = lineUnit == "lb"? String.format("%.1f", Line.convert(newPower, "lb")) + "ho" : String.format("%.1f", newPower) + "ho";
+
+        // string stream for result string
+        StringBuilder sb = new StringBuilder();             
+        sb.append(newPowerHoStr + "-" + newPowerLbStr  + " " + newCapaStr + LENGTH_UNIT);
+
+        // conditionally added text (lb 기준으로 작동함. ho로 입력받은 인스턴스도 상관없이 lb기준으로 작동.)
+        boolean isDefaultPower = getBasePowerLb() == newPower;
+        boolean isDefferenceOver = getBasePowerLb() >= newPower + DEFFERENCE_OVER || getBasePowerLb() <= newPower - DEFFERENCE_OVER;
+        if (isDefferenceOver)       { sb.append(" (오차주의)"); }
+        if (isDefaultPower)         { sb.append(" (Base)"); }
+
+        // print result.
+        System.out.println(sb.toString());
     }
 
-    // simple print other power line.
-    public void printOtherLine(double targetPower, String lineUnit) {
-        double res = getOtherLineCapa(targetPower, lineUnit);
-        String basePowerStr = String.format("%.0flb", getBasePowerLb());
-        String targetPowerStr = String.format("%.0flb", targetPower);
-        String resStr = String.format("%.1f", res);
-        System.out.print(targetPowerStr + " " + resStr + LENGTH_UNIT);
-        if (getBasePowerLb() >= targetPower + 6 || getBasePowerLb() <= targetPower - 6) {
-            System.out.print("  오차주의: 기본 " + basePowerStr + "과 6lb 이상 차이남");
-        }
-        if (getBasePowerLb() == targetPower) { System.out.print(" (기본값)"); }
-        System.out.println();
-    }
-
-    // 자주쓰는 lb 4~20 까지 모두 출력
+    // printOtherLine() overloading. print 4lb ~ 20lb (step 2lb)
     public void printOtherLine() {
         int startPoint = 4;
         int endPoint = 20;
